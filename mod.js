a99437111fdc2f7f145c9af753f4fe60dd4b6b04
Daniel <lmao@distruzione.org>
2022-03-28 19:16:07 +0300

    Fixes #3

--- mod.js
+++ mod.js
@@ -2,11 +2,11 @@ import init, {initThreadPool, decode} from "./wasm/wasm_image_decoder.js";
 await init();
 await initThreadPool(4);
 
-export default function(arrayBuffer) {
-  if(arrayBuffer.constructor !== ArrayBuffer) {
-    throw new Error("Expecting an arraybuffer as input");
+export default function(buffer) {
+  if(buffer.constructor !== Uint8Array) {
+    throw new Error("Expecting an Uint8Array as input");
   }
-  let result = decode(new Uint8Array(arrayBuffer));
+  let result = decode(buffer);
   let data = result.slice(0, -4);
   let widthBytes = result.slice(-4);
   let width = (widthBytes[0] << 24) + (widthBytes[1] << 16) + (widthBytes[2] << 8) + (widthBytes[3] << 0);
